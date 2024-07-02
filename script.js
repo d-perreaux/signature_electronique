951d7f5ab5d1ba4a9c40f2a97b17ddd5d8883b67
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2024-07-02 12:17:14 +0200

    feat(canvas-signature): add complete logic to upload an image of a signature

---
+++ script.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const canvas = document.querySelector("canvas");
+    const form = document.querySelector(".signature-pad-form");
+    const clearButton = document.querySelector(".clear-button");
+    const ctx = canvas.getContext("2d");
+    let writingMode = false;
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const imageURL = canvas.toDataURL();
+        const image = document.createElement("img");
+        image.src = imageURL;
+        image.height = canvas.height;
+        image.width = canvas.width;
+        image.style.display = "block";
+        form.appendChild(image);
+        clearPad;
+    })
+
+    const clearPad = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    clearButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        clearPad();
+    })
+
+    getTargetPosition = (event) => {
+        positionX = event.clientX - event.target.getBoundingClientRect().x;
+        positiony = event.clientY - event.target.getBoundingClientRect().y;
+
+        return [positionX, positiony];
+    }
+
+    handlePointerMove = (event) => {
+        if (!writingMode) return
+
+        const [positionX, positionY] = getTargetPosition(event);
+        ctx.lineTo(positionX, positionY);
+        ctx.stroke();
+    }
+
+    handlePointerUp = () => {
+        writingMode = false;
+    }
+
+    handlePointerDown = (event) => {
+        writingMode = true;
+        ctx.beginPath();
+
+        const [positionX, positionY] = getTargetPosition(event);
+        ctx.moveTo(positionX, positionY);
+    }
+
+    ctx.lineWidth = 2;
+    ctx.lineJoin = ctx.lineCap = "round";
+
+    canvas.addEventListener("pointerdown", handlePointerDown, {passive: true});
+    canvas.addEventListener("pointerup", handlePointerUp, {passive: true});
+    canvas.addEventListener("pointermove", handlePointerMove, {passive: true});
+})
